e6b3b52ca90ea4fbfdff384d6fb1cdc5cbc3c42a
TheHolyOrange <144804518+TheHolyOrange@users.noreply.github.com>
2023-12-26 18:22:23 +0530

    Delete update.py

--- update.py
+++
@@ -1,6 +0,0 @@
-import PatientMaintenance
-
-def update():
-    patientId=input("Enter the ID you want to change: ")
-    patientName=input("Enter the name of the updated patient: ")
-    print(PatientMaintenance.updatePatient(patientId,patientName))
